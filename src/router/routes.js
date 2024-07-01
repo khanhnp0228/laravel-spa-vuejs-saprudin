bf9ecd721d9fc780799ca63fd4e70239a256ba59
Khanh Nguyen <khanh@propolife.co.jp>
2024-07-01 16:01:01 +0700

    Handling 404 error

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,8 @@ import TaskPage from '../pages/TaskPage.vue'
 import SummaryPage from '../pages/SummaryPage.vue'
 import LoginPage from '../pages/LoginPage.vue'
 import RegisterPage from '../pages/RegisterPage.vue'
+import NotFoundErrorPage from '../pages/errors/NotFoundErrorPage.vue'
+
 
 const routes = [
     {
@@ -23,6 +25,11 @@ const routes = [
         path: '/register',
         component: RegisterPage,
         name: 'register'
+    },
+    {
+        path: '/:notFound(.*)',
+        component: NotFoundErrorPage,
+        name: 'error.404'
     },
 ]
 
