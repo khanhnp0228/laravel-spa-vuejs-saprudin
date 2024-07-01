f876df125a888e77bb696f6d9e75f32c5160fade
Khanh Nguyen <khanh@propolife.co.jp>
2024-07-01 14:08:42 +0700

    Refactoring our Stores (Using Setup stores)

--- src/stores/task.js
+++ src/stores/task.js
@@ -1,25 +1,19 @@
 import {defineStore} from "pinia";
 import {allTasks} from "../http/task-api";
+import {computed, ref} from "vue";
 
-export const useTaskStore = defineStore('taskStore', {
-    state: () => ({
-        tasks: [],
-        task: {
-            id: 1,
-            name: 'First task',
-            is_completed: false,
-        }
-    }),
-    getters: {
-        completedTasks: (state) => state.tasks.filter(task => task.is_completed),
-        uncompletedTasks () {
-            return this.tasks.filter(task => !task.is_completed)
-        }
-    },
-    actions: {
-        async fetchAllTasks () {
-            const {data} = await allTasks()
-            this.tasks = data.data
-        }
+
+export const useTaskStore = defineStore('taskStore', () => {
+    const tasks = ref([])
+    const completedTasks = computed(() => tasks.value.filter(task => task.is_completed))
+    const uncompletedTasks = computed(() => tasks.value.filter(task => !task.is_completed))
+
+    const fetchAllTasks = async () => {
+        const {data} = await allTasks()
+        tasks.value = data.data
+    }
+
+    return {
+        tasks, completedTasks, uncompletedTasks, fetchAllTasks
     }
 })
