34b75ea59a1b9eb0ac6c16d77a99e151f46def8b
Khanh Nguyen <khanh@propolife.co.jp>
2024-07-03 14:48:37 +0700

    Adding additional options to the SummaryFilter component

--- src/components/summaries/filter/SummaryFilter.vue
+++ src/components/summaries/filter/SummaryFilter.vue
@@ -4,8 +4,7 @@
             <DropdownTrigger class="btn btn-outline-secondary" :class="toggleClass" type="button" @click="toggle">Filter</DropdownTrigger>
         </template>
         <template v-slot:menu="{ toggle }">
-            <DropdownItem href="#" @click.prevent="toggle">Today</DropdownItem>
-            <DropdownItem href="#" @click.prevent="toggle">Yesterday</DropdownItem>
+            <DropdownItem v-for="(value, key) in filterItems" :key="key" href="#" @click.prevent="toggle">{{ value }}</DropdownItem>
         </template>
     </Dropdown>
 </template>
@@ -14,6 +13,25 @@
 import Dropdown from "../../dropdown/Dropdown.vue";
 import DropdownItem from "../../dropdown/DropdownItem.vue";
 import DropdownTrigger from "../../dropdown/DropdownTrigger.vue";
+import {computed} from "vue";
+import { startOfWeek, endOfWeek, subWeeks, format } from 'date-fns';
+
+const filterItems = computed(() => {
+    const thisWeekStart = format(startOfWeek(new Date()), 'd MMM')
+    const thisWeekEnd = format(endOfWeek(new Date()), 'd MMM')
+    const lastWeekStart = format(startOfWeek(subWeeks(new Date(), 1)), 'd MMM')
+    const lastWeekEnd = format(endOfWeek(subWeeks(new Date(), 1)), 'd MMM')
+
+    return {
+        'today': 'Today',
+        'yesterday': 'Yesterday',
+        'thisweek': `This week (${thisWeekStart} - ${thisWeekEnd})`,
+        'lastweek': `Last week (${lastWeekStart} - ${lastWeekEnd})`,
+        'thismonth': 'This month',
+        'lastmonth': 'Last month'
+
+    }
+})
 </script>
 
 <style scoped>
